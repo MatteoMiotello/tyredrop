87a314c6c1bfbd9b65f3a586f3f0a6288e7e9262
matteo_miotello <matteo.miotello@shellrent.com>
2023-02-04 19:28:09 +0100

    user and refresh token migrations

---
+++ migrations/20230204192326_refreshtoken.go
@@ -0,0 +1,34 @@
+package migrations
+
+import (
+	"database/sql"
+	"github.com/MatteoMiotello/go-sqlbuilder"
+	"github.com/MatteoMiotello/go-sqlbuilder/types"
+	"github.com/pressly/goose/v3"
+)
+
+func init() {
+	goose.AddMigration(upRefreshtoken, downRefreshtoken)
+}
+
+func upRefreshtoken(tx *sql.Tx) error {
+	refreshTokenQuery := sqlbuilder.CreateTable("public.refresh_tokens").
+		PKColumn().
+		FKColumn("public.users", "user_id", false).
+		Column("refresh_token", types.Varchar.Options("500"), false).
+		Column("time_last_use", types.Timestamptz, true).
+		DeletedColumn().
+		CreatedColumn().
+		String()
+
+	_, err := tx.Exec(refreshTokenQuery)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func downRefreshtoken(tx *sql.Tx) error {
+	// This code is executed when the migration is rolled back.
+	return nil
+}
